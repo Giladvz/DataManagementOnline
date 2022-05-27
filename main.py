4f5eff3079624136a2eefa582cbb858394622502
Gilad Vardy-zer <gilad.vardyzer@taranis.ag>
2022-05-27 19:19:24 +0300

    Changes

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import queue
 g = rdflib.Graph()
 r = requests.get("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")
 doc = lxml.html.fromstring(r.content)
-urls_queue = queue.Queue()
+countries_queue = queue.Queue()
+prime_Pres_queue = queue.Queue()
 prefix = "http://en.wikipedia.org"
+RDF_URI_PREFIX = "http://example.org/"
 labels_for_country = ["Prime Minister","President","Government","capital","Area"]
 lables_for_Persons = ["Born"]
 visited = set()
@@ -15,10 +17,15 @@ visited = set()
 
 def push_to_queue(t, entitype):
     if (f"{prefix}{t}" not in visited):
-        urls_queue.put((entitype, f"{prefix}{t}"))
+        countries_queue.put((entitype, f"{prefix}{t}"))
         visited.add(f"{prefix}{t}")
 
-def crawl(queue):
+def create_ontology(que):
+    g =  rdflib.Graph()
+    while not que.empty():
+        country = que.pop()
+        countryname = country[5:]
+        country_literal = rdflib.URIRef(RDF_URI_PREFIX + countryname)
 
 
 
@@ -30,12 +37,26 @@ def import_countries():
         if (t not in states and not t.startswith('#') ):
             states.append(t)
             if (f"{prefix}{t}" not in visited):
-                urls_queue.put(("Country", f"{prefix}{t}"))
+                countries_queue.put(("Country", f"{prefix}{t}"))
                 visited.add(f"{prefix}{t}")
             print(count, t)
             count += 1
+    for t in doc.xpath('//table[@class="wikitable sortable static-row-numbers plainrowheaders srn-white-background"]//tr/td[1]/a[1]/@href'):
+        states.append(t)
+        print(t)
+    count = 1
+    for t in doc.xpath('//table[@class="wikitable sortable static-row-numbers plainrowheaders srn-white-background"]//tr/td[1]/span/a[1]/@href'):
+        states.append(t)
+        print(count, t)
+        count+=1
+
     print(count)
-    crawl(urls_queue)
+    print("/wiki/French_Fifth_Republic" in states)
+    ##crawl(countries_queue)
+
+
+import_countries()
+
 
 
 
