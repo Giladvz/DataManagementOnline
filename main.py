0065f68c702fcf4f4ca7d9c6c14b63d8abbe020b
Gilad Vardy-zer <gilad.vardyzer@taranis.ag>
2022-05-28 00:13:13 +0300

    changed the code up a bit, about to erase a lot

--- main.py
+++ main.py
@@ -11,47 +11,41 @@ xpaths = [{"/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[th/a/text()
        {"/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[./th/a[text()='Government']]/td/a/text() | tr[./th/a[text()='Government']]/td/span/a/text() | tr[./th/text()='Government']/td/a/text()",True,"Government Form"}
       ]
 
-def create_ontology(que):
-    g = rdflib.Graph()
-    while not que.empty():
-        country = que.pop()
+r = requests.get("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")
+doc = lxml.html.fromstring(r.content)
+countries_queue = queue.Queue()
+prefix = "http://en.wikipedia.org"
+RDF_URI_PREFIX = "http://example.org/"
+labels_for_country = ["Prime Minister", "President", "Government", "capital", "Area"]
+labels_for_Persons = ["Born"]
+
+
+def create_ontology():
+    g = Graph()
+    import_countries()
+    while not countries_queue.empty():
+        country = countries_queue.get()
         countryname = country[5:]
-        country_literal = rdflib.URIRef(RDF_URI_PREFIX + countryname)
+        country_literal = URIRef(RDF_URI_PREFIX + countryname)
         for xp in xpaths[:5]:
             add_to_ontology(countryname, xp[0], xp[1], xp[2], g)
 
 
 def add_to_ontology(name, xpath, spaces,literal,g):
     for elem in self.doc.xpath(xpath):
-        if(spaces):
+        if spaces:
             g.add((country, URIRef(RDF_URI_PREFIX + literal), Literal(str(elem).replace(" ", ""))))
         else:
             g.add((country, URIRef(RDF_URI_PREFIX + literal), Literal(str(elem))))
 
 
 
-r = requests.get("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")
-doc = lxml.html.fromstring(r.content)
-countries_queue = queue.Queue()
-prime_Pres_queue = queue.Queue()
-prefix = "http://en.wikipedia.org"
-RDF_URI_PREFIX = "http://example.org/"
-labels_for_country = ["Prime Minister","President","Government","capital","Area"]
-lables_for_Persons = ["Born"]
-visited = set()
 
 
 def push_to_queue(t, entitype):
-    if (f"{prefix}{t}" not in visited):
+#    if (f"{prefix}{t}" not in visited):
         countries_queue.put((entitype, f"{prefix}{t}"))
-        visited.add(f"{prefix}{t}")
-
-def create_ontology(que):
-    g = Graph()
-    while not que.empty():
-        country = que.pop()
-        countryname = country[5:]
-        country_literal = URIRef(RDF_URI_PREFIX + countryname)
+ #       visited.add(f"{prefix}{t}")
 
 
 
@@ -69,15 +63,12 @@ def import_countries():
         if (t not in states and not t.startswith('#') ):
             count += 1
             states.append(t)
-            if (f"{prefix}{t}" not in visited):
-                countries_queue.put(("Country", f"{prefix}{t}"))
-                visited.add(f"{prefix}{t}")
+            push_to_queue(t,"country")
             print(count, t)
     print(count)
-    print("/wiki/French_Fifth_Republic" in states)
     ##crawl(countries_queue)
 
-
+print(" Hello World              ".strip())
 import_countries()
 
 
