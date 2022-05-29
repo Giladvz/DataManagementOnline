c6e533c266e5bcc202b1b2fa3a314d5472097e5c
Gilad Vardy-zer <gilad.vardyzer@taranis.ag>
2022-05-29 03:40:58 +0300

    population + area + capital work

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ import requests
 import queue
 import re
 
-xpaths = [["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Population']/following::td[1]/text()[1]","Population"],
-               ["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='President']/td//a[1][not(contains(@href,'#cite'))]/text()","President"],
-               ["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Prime Minister']/td/a[1][not(contains(@href,'#cite'))]/text()","Prime_Minister"],
-               ["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Area ']/following::td[1]/text()[1]" ,"Area"],
-               ["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Capital']/td/a[1][not(contains(@href,'#cite'))]/@href","Capital"],
-               ["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Government']/td//a[not(contains(@href,'#cite'))]/@href","Government_Form"]]
+xpaths = [#["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Population']/td/text()[1] | "
+           #"//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Population']/following::td[1]/div/ul/li[1]/text()[1] | "
+           #"//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Population']/following::td[1]/text()[1] | "
+           #"//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Population']/following::td[1]/span/text()[1] | "
+           #"//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Population']/following::td[1]/span/span/text() | "
+           #"//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Population']/following::td[1]/i/text()","Population"],
+               #["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='President']/td//a[1][not(contains(@href,'#cite'))]/text()","President"],
+               #["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Prime Minister']/td/a[1][not(contains(@href,'#cite'))]/text()","Prime_Minister"],
+               ["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Area ']/following::td[1]/text()[1] | "
+                "//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Area']/following::td[1]/text()[1] | "
+                "//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Area']/td/text()[1]", "Area"],
+               ["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Capital']/td//a[1][not(contains(@href,'#cite'))]/@href","Capital"],
+               ]#["//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Government']/td//a[not(contains(@href,'#cite'))]/@href","Government_Form"]]
 
-
-
-
-
-
-##pipi's xpaths
+## people's xpaths
 p_birth_date_xpath = "//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Born']//span[@class='bday']//text()"
-
 p_birth_place_xpath = "//table[contains(@class, 'infobox')]/tbody/tr[th//text()='Born']//td[1]//text()"
 
 states = []
@@ -29,20 +30,68 @@ prefix = "http://en.wikipedia.org/"
 RDF_URI_PREFIX = "http://example.org/"
 labels_for_country = ["Prime_Minister", "President", "Government_Form", "capital", "Area", "Population"]
 labels_for_Persons = ["Born"]
-
+g = Graph()
+count = 0
 
 def create_ontology():
-    g = Graph()
     import_countries()
+    for i in range(220):
+        countries_queue.get()
     while not countries_queue.empty():
         country = countries_queue.get()
         for xp in xpaths:
             add_to_ontology(country, xp[0], xp[1], g)
-    g.serialize(destination='ontology.nt', format='nt')
+    print(count)
+    g.serialize(destination='ontology.nt', format='nt', encoding="utf-8", errors="ignore")
 
 
+def import_countries():
+    r = requests.get("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")
+    doc = lxml.html.fromstring(r.content)
+    for t in doc.xpath('//table[@class="wikitable sortable static-row-numbers plainrowheaders srn-white-background"]//tr/td[1]/span/a[1]/@href |'
+                       '//table[@class="wikitable sortable static-row-numbers plainrowheaders srn-white-background"]//tr/td[1]/i/a[1]/@href | '
+                       '//table[@class="wikitable sortable static-row-numbers plainrowheaders srn-white-background"]//tr/td[1]/span[@class = "flagicon"]/following-sibling::a[1]/@href'):
+        if (t not in states and not t.startswith('#') ):
+            states.append(t[6:].replace("_", " "))
+            countries_queue.put(t)
 
-##Adding pipi's info to the Ontology
+
+def add_to_ontology(country, xpath, literal, g):
+    global count
+    r = requests.get(prefix + country)
+    doc = lxml.html.fromstring(r.content)
+    # If doc.xpath(xpath) is empty, doesn't go in
+    for elem in doc.xpath(xpath):
+        elem = elem.strip()
+        elem = elem.replace(" ", "_")
+        elem = urllib.parse.quote(elem)
+        if (literal in ["Prime_Minister", "President"]):
+            get_prim_and_pres(elem, g)
+            g.add((URIRef(str(RDF_URI_PREFIX+elem).strip()), URIRef(RDF_URI_PREFIX + literal), URIRef(RDF_URI_PREFIX + country[5:])))
+            print(RDF_URI_PREFIX + literal + " of " + RDF_URI_PREFIX + country[5:] + " is " + Literal(str(RDF_URI_PREFIX+elem).strip()))
+        elif (literal == "Capital"):
+            g.add((URIRef(str(RDF_URI_PREFIX+elem[5:]).strip()), URIRef(RDF_URI_PREFIX + literal), URIRef(RDF_URI_PREFIX + country[5:])))
+            print(RDF_URI_PREFIX + literal + " of " + RDF_URI_PREFIX + country[5:] + " is " + Literal(str(RDF_URI_PREFIX+elem[5:]).strip()))
+            count += 1
+            break
+        elif (literal in ["Area", "Population"]):
+            elem = urllib.parse.unquote(elem)
+            if elem == "" or elem == " " or elem == " (" or elem == "(":
+                continue
+            #print(elem)
+            elem = re.match("^([-0-9,\.–]+).*",elem)
+            elem = elem.group(1)
+            g.add((Literal(str(elem).strip()), URIRef(RDF_URI_PREFIX + literal), URIRef(RDF_URI_PREFIX + country[5:])))
+            print(RDF_URI_PREFIX + literal + " of " + RDF_URI_PREFIX + country[5:] + " is " + Literal(str(elem).strip()))
+            count+=1
+            break
+        else:
+            g.add((URIRef(str(RDF_URI_PREFIX+elem[5:]).strip()), URIRef(RDF_URI_PREFIX + literal), URIRef(RDF_URI_PREFIX + country[5:])))
+            elem = urllib.parse.unquote(elem)
+            print(RDF_URI_PREFIX + literal + " of " + RDF_URI_PREFIX + country[5:] + " is " + Literal(str(RDF_URI_PREFIX+elem).strip()))
+
+
+##Adding people's info to the Ontology
 def get_prim_and_pres(p, g):
     url = prefix + "/wiki/" + p
     doc = lxml.html.fromstring((requests.get(url).content))
@@ -56,8 +105,8 @@ def get_prim_and_pres(p, g):
                URIRef(RDF_URI_PREFIX + p.replace(" ", "_"))))
     for elem in doc.xpath(p_birth_date_xpath):
         if (elem):
-            print(Literal(str(RDF_URI_PREFIX + elem).strip()) + " is the " + RDF_URI_PREFIX + "Birth_Date" + " of " + RDF_URI_PREFIX + p.replace(" ", "_"))
-            g.add((Literal(str(RDF_URI_PREFIX + elem).strip()), URIRef(RDF_URI_PREFIX + "Birth_Date"),
+            print(str(RDF_URI_PREFIX + elem).strip() + " is the " + RDF_URI_PREFIX + "Birth_Date" + " of " + RDF_URI_PREFIX + p.replace(" ", "_"))
+            g.add((str(RDF_URI_PREFIX + elem).strip(), URIRef(RDF_URI_PREFIX + "Birth_Date"),
                    URIRef(RDF_URI_PREFIX + p.replace(" ", "_"))))
 
 
@@ -66,54 +115,6 @@ def search_for_country(elem):
         if(country in str(elem)):
             return country
     return None
-
-
-
-
-def add_to_ontology(country, xpath, literal, g):
-    r = requests.get(prefix + country)
-    doc = lxml.html.fromstring(r.content)
-    for elem in doc.xpath(xpath):
-        if elem == None:
-            print(country, literal, " is empty")
-            break
-        elem = elem.strip()
-        elem = elem.replace(" ", "_")
-        elem = urllib.parse.quote(elem)
-        if (literal in ["Prime_Minister", "President"]):
-            get_prim_and_pres(elem, g)
-            g.add((URIRef(str(RDF_URI_PREFIX+elem).strip()), URIRef(RDF_URI_PREFIX + literal), URIRef(RDF_URI_PREFIX + country[5:])))
-            print(RDF_URI_PREFIX + literal + " of " + RDF_URI_PREFIX + country[5:] + " is " + Literal(str(RDF_URI_PREFIX+elem).strip()))
-        elif (literal == "Capital"):
-            g.add((URIRef(str(RDF_URI_PREFIX+elem[5:]).strip()), URIRef(RDF_URI_PREFIX + literal), URIRef(RDF_URI_PREFIX + country[5:])))
-            print(RDF_URI_PREFIX + literal + " of " + RDF_URI_PREFIX + country[5:] + " is " + Literal(str(RDF_URI_PREFIX+elem[5:]).strip()))
-        elif (literal in ["Area", "Population"]):
-            elem = urllib.parse.unquote(elem)
-            elem = re.match("^(\d+).*",elem)
-            print(elem.group(1))
-            g.add((Literal(str(elem).strip()), URIRef(RDF_URI_PREFIX + literal), URIRef(RDF_URI_PREFIX + country[5:])))
-            print(RDF_URI_PREFIX + literal + " of " + RDF_URI_PREFIX + country[5:] + " is " + Literal(str(elem).strip()))
-        else:
-            g.add((URIRef(str(RDF_URI_PREFIX+elem[5:]).strip()), URIRef(RDF_URI_PREFIX + literal), URIRef(RDF_URI_PREFIX + country[5:])))
-            elem = urllib.parse.unquote(elem)
-            print(RDF_URI_PREFIX + literal + " of " + RDF_URI_PREFIX + country[5:] + " is " + Literal(str(RDF_URI_PREFIX+elem).strip()))
-
-
-
-def import_countries():
-    count = 0
-    r = requests.get("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")
-    doc = lxml.html.fromstring(r.content)
-    for t in doc.xpath('//table[@class="wikitable sortable static-row-numbers plainrowheaders srn-white-background"]//tr/td[1]/span/a[1]/@href |'
-                       '//table[@class="wikitable sortable static-row-numbers plainrowheaders srn-white-background"]//tr/td[1]/i/a[1]/@href | '
-                       '//table[@class="wikitable sortable static-row-numbers plainrowheaders srn-white-background"]//tr/td[1]/span[@class = "flagicon"]/following-sibling::a[1]/@href'):
-        if (t not in states and not t.startswith('#') ):
-            count += 1
-            states.append(t[6:].replace("_", " "))
-            countries_queue.put(t)
-            print(count, t)
-    print(states)
-    ##crawl(countries_queue)
 
 
 create_ontology()
